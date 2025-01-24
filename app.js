ad9ddb82c16f1cf1ddb4143da315cdfce997a54e
Morgan <morganridgef@gmail.com>
2025-01-23 19:54:01 -0500

    planned project awaiting approval

---
+++ app.js
@@ -0,0 +1,47 @@
+// Please read the readme as well!
+
+// Psuedocode
+
+//1 I'll define all of the required variables, leaving any empty as needed. 
+
+//2 I'll store the cached element references using getbyID.
+
+//3 I'll create the initialize game and render functions. 
+
+//4 I'll handle each instead of the player clicking a button using a handleclick function.
+
+//5 I'll create a timer to allow only a certain amount of time for the player to make their choices.
+
+//6 I'll code a way for the player to turn over the cards, which will change the image from the back of the pokemon card with the pokemon logo to the front with pokemon on it.
+
+//7 I'll code a way for the program to recognize when a match has been made.
+
+//8 I'll implement a way for the the player to only keep playing until the timer is up or until they have made too many incorrect matches.
+
+//9 I'll code the message feature so that the player is able to view the current state of the game, such as whether their match was correct or incorrect, to keep going, or if they've lost.
+
+//10 I'll code my reset function so that the player can try again. 
+
+//11 I'll make sure the render and initialize features work. 
+
+//12 I'll make sure everything is running properly and that there are no errors. 
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// I need to think about how the player will win or lose the game. Whether it will be time based or if it will be based on the number of correct or incorrect matches.
+
+// I need to think about whether every time you reset the game if it will show the same cards or instead, new cards from a set amount of cards. 
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Graveyard --------------------------------*/
---
+++ projectplanning.md
@@ -0,0 +1,16 @@
+# pokemon-memory-game
+## A Pokemon Memory Card Game 
+
+![Pink gameboy with pokemon game](./assets/pink-gameboy.jpg)
+
+### Planning
+
+1. What I've done so far is created all of the files needed for my project and put in the boilerplate on my HTML document. 
+
+2. The first thing I'll get started on after approval is my html. I'll write my HTML document allowing for a grid of pokemon cards. I'll need to gather my assets to do this including images of the pokemon cards. 
+
+3. The next thing I'll do is work on my styling in css. I'll add my background imagine and change the colors of things. I'll make a grid using flexbox so all of the pokemon cards are where I need them to be. I'll also style my buttons and any other visual features.
+
+4. Then, I will write my javascript adding functionality to my game from what I have learned during the last two games I've coded. I'll have to decide what makes the player win or lose the game, such as will there be a timer, or will too many wrong matches lose the game? I'll have to code a way to turn the cards over, and only allow two cards to be turned over at once. I'll have to code the message telling the user that their match is correct or incorrect, and if they've won or lost. And I'll have to code a reset game function, and many other things.
+
+5. Lastly, I will make sure my code is working properly with no errors, and then I will deploy my site and submit my project. 
